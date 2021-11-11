e73ea6370ed998e85989a933fe47da61cc409743
asnabryg <8mixu8@gmail.com>
2021-11-11 22:42:31 +0200

    pylint ja coverage lisätty

--- tasks.py
+++ tasks.py
@@ -1,10 +1,21 @@
 from invoke import task
-from invoke.tasks import Task
 
 @task
 def start(ctx):
     ctx.run("python3 src/index.py")
 
-@Task
+@task
 def test(ctx):
     ctx.run("pytest src")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
+
+@task
+def lint(ctx):
+    ctx.run("pylint src")
